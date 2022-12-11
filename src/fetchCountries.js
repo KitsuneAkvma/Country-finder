6597da7738243f8bee01a659bddf5e2372ee7b00
Mateusz Martin <mateusz.martinn@gmail.com>
2022-12-11 14:11:55 +0100

    country-finder\partials && \improvments
    Added specific actions that should happen when the list of found countries is too long or when the user entered a name that returns only one result

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,1 +1,110 @@
 import { country } from './index.js';
+
+const searchBar = document.querySelector('.search__box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+// to search for countries with not specified country name
+export function fetchCountries(url) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      countryList.innerHTML = '';
+      if (data.length > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        return;
+      } else if (data.length > 1 && data.length <= 10) {
+        data.forEach(element => {
+          let country = document.createElement('li');
+          let countryNam = document.createElement('div');
+          let countryFlag = document.createElement('div');
+          // let countryFound = document.createTextNode(`${element.flag} ${element.name.common}`);
+          // country.appendChild(countryFound);
+          country.classList.add('country-list__item');
+          countryNam.classList.add(`country-list__item--name`);
+          countryFlag.classList.add(`country-list__item--flag`);
+
+          countryNam.innerHTML = `${element.name.common}`;
+          countryFlag.innerHTML = `${element.flag}`;
+
+          country.appendChild(countryFlag);
+          country.appendChild(countryNam);
+          countryList.appendChild(country);
+          let selectedCountry;
+
+          document.querySelectorAll('.country-list__item').forEach(element =>
+            element.addEventListener('click', event => {
+              selectedCountry = event.currentTarget.querySelector(
+                '.country-list__item--name'
+              ).innerHTML;
+              fetchCountry(`https://restcountries.com/v3.1/name/${selectedCountry}`);
+              searchBar.value = '';
+              countryList.innerHTML = '';
+            })
+          );
+        });
+      } else {
+        fetchCountry(`https://restcountries.com/v3.1/name/${data[0].name.common}`);
+      }
+    })
+    .catch(error => {
+      if (countryName.length == 0) {
+      } else {
+        Notiflix.Notify.failure(`Oops, there is no country with that name`);
+        console.log(error);
+      }
+    });
+}
+
+// to use with specified country name
+function fetchCountry(url) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      countryInfo.innerHTML = `<div class="country-info__wrapper">
+                  <div class="country-info__symbols">
+                    <img src="${
+                      data[0].flags.svg
+                    }" alt="National flag" class="country-info__flag" />
+                    <img src="${
+                      data[0].coatOfArms.svg
+                    }" alt="Coat of Arms" class="country-info__emblem" />
+                  </div>
+                  <div class="country-info__data">
+                    <h2 class="country-info__common-name">${data[0].name.common}</h2>
+                    <div class="country-info__data__wrapper">
+                      <h3 class="data--name">Alternative spellings</h3>
+                      <p class="data--content">${data[0].altSpellings
+                        .toString()
+                        .split(',')
+                        .join(', ')}</p>
+                    </div>
+                    <div class="country-info__data__wrapper">
+                      <h3 class="data--name">Languages</h3>
+                      <p class="data--content">${Object.values(data[0].languages)}</p>
+                    </div>
+                    <div class="country-info__data__wrapper">
+                      <h3 class="data--name">Currency</h3>
+                      <p class="data--content">${Object.keys(data[0].currencies)}</p>
+                    </div>
+                    <div class="country-info__data__separator"></div>
+                    <div class="country-info__data__wrapper">
+                      <h3 class="data--name">Capital</h3>
+                      <p class="data--content">${data[0].capital[0]}</p>
+                    </div>
+                    <div class="country-info__data__wrapper">
+                      <h3 class="data--name">Continent</h3>
+                      <p class="data--content">${data[0].continents[0]}</p>
+                    </div>
+                    <div class="country-info__data__wrapper">
+                      <h3 class="data--name">Timezone</h3>
+                      <p class="data--content">${data[0].timezones[0]}</p>
+                    </div>
+                    <div class="country-info__data__wrapper">
+                      <h3 class="data--name">Population</h3>
+                      <p class="data--content">${Intl.NumberFormat().format(data[0].population)}</p>
+                    </div>
+                  </div>
+                </div>`;
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
 
 // Defining DOM elements
 
 const searchBar = document.querySelector('.search__box');
-const searchButton = document.querySelector('.search__button');
+// const searchButton = document.querySelector('.search__button');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 searchBar.addEventListener(
@@ -17,94 +18,7 @@ searchBar.addEventListener(
     }
     let countryName = searchBar.value;
     let finalURL = `https://restcountries.com/v3.1/name/${countryName}`;
-    fetch(finalURL)
-      .then(response => response.json())
-      .then(data => {
-        countryList.innerHTML = '';
-        data.forEach(element => {
-          let country = document.createElement('li');
-          let countryNam = document.createElement('div');
-          let countryFlag = document.createElement('div');
-          // let countryFound = document.createTextNode(`${element.flag} ${element.name.common}`);
-          // country.appendChild(countryFound);
-          country.classList.add('country-list__item');
-          countryNam.classList.add(`country-list__item--name`);
-          countryFlag.classList.add(`country-list__item--flag`);
-
-          countryNam.innerHTML = `${element.name.common}`;
-          countryFlag.innerHTML = `${element.flag}`;
-
-          country.appendChild(countryFlag);
-          country.appendChild(countryNam);
-          countryList.appendChild(country);
-
-          let selectedCountry;
-          document.querySelectorAll('.country-list__item').forEach(element =>
-            element.addEventListener('click', event => {
-              selectedCountry = event.currentTarget.querySelector(
-                '.country-list__item--name'
-              ).innerHTML;
-              searchBar.value = '';
-              countryList.innerHTML = '';
-              fetch(`https://restcountries.com/v3.1/name/${selectedCountry}`)
-                .then(response => response.json())
-                .then(data => {
-                  countryInfo.innerHTML = `<div class="country-info__wrapper">
-                <div class="country-info__symbols">
-                  <img src="${data[0].flags.svg}" alt="National flag" class="country-info__flag" />
-                  <img src="${
-                    data[0].coatOfArms.svg
-                  }" alt="Coat of Arms" class="country-info__emblem" />
-                </div>
-                <div class="country-info__data">
-                  <h2 class="country-info__common-name">${data[0].name.common}</h2>
-                  <div class="country-info__data__wrapper">
-                    <h3 class="data--name">Alternative spellings</h3>
-                    <p class="data--content">${data[0].altSpellings
-                      .toString()
-                      .split(',')
-                      .join(', ')}</p>
-                  </div>
-                  <div class="country-info__data__wrapper">
-                    <h3 class="data--name">Languages</h3>
-                    <p class="data--content">${Object.values(data[0].languages)}</p>
-                  </div>
-                  <div class="country-info__data__wrapper">
-                    <h3 class="data--name">Currency</h3>
-                    <p class="data--content">${Object.keys(data[0].currencies)}</p>
-                  </div>
-                  <div class="country-info__data__separator"></div>
-                  <div class="country-info__data__wrapper">
-                    <h3 class="data--name">Capital</h3>
-                    <p class="data--content">${data[0].capital[0]}</p>
-                  </div>
-                  <div class="country-info__data__wrapper">
-                    <h3 class="data--name">Continent</h3>
-                    <p class="data--content">${data[0].continents[0]}</p>
-                  </div>
-                  <div class="country-info__data__wrapper">
-                    <h3 class="data--name">Timezone</h3>
-                    <p class="data--content">${data[0].timezones[0]}</p>
-                  </div>
-                  <div class="country-info__data__wrapper">
-                    <h3 class="data--name">Population</h3>
-                    <p class="data--content">${Intl.NumberFormat().format(data[0].population)}</p>
-                  </div>
-                </div>
-              </div>`;
-                });
-            })
-          );
-        });
-      })
-      .catch(error => {
-        if (countryName.length == 0) {
-          console.log('field is empty');
-        } else {
-          Notiflix.Notify.failure(`${error}`);
-          console.log(error);
-        }
-      });
+    fetchCountries(finalURL);
   }, 300)
 );
 // // Variables
